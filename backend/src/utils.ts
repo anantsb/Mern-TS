ed94eb9bab936943527dd3eabe046070a05f211d
anantsb <anantbhagat44@gmail.com>
2023-05-23 19:24:29 +0530

    Changes

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -1,6 +1,6 @@
-import { NextFunction, Request, Response } from 'express'
-import jwt from 'jsonwebtoken'
-import { User } from './models/userModel'
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { User } from "./models/userModel";
 
 export const generateToken = (user: User) => {
   return jwt.sign(
@@ -10,30 +10,30 @@ export const generateToken = (user: User) => {
       email: user.email,
       isAdmin: user.isAdmin,
     },
-    process.env.JWT_SECRET || 'somethingsecret',
+    process.env.JWT_SECRET || "secret",
     {
-      expiresIn: '30d',
+      expiresIn: "30d",
     }
-  )
-}
+  );
+};
 
 export const isAuth = (req: Request, res: Response, next: NextFunction) => {
-  const { authorization } = req.headers
+  const { authorization } = req.headers;
   if (authorization) {
-    const token = authorization.slice(7, authorization.length) // Bearer xxxxx
+    const token = authorization.slice(7, authorization.length); // Bearer xxxxx
     const decode = jwt.verify(
       token,
-      process.env.JWT_SECRET || 'somethingsecret'
-    )
+      process.env.JWT_SECRET || "somethingsecret"
+    );
     req.user = decode as {
-      _id: string
-      name: string
-      email: string
-      isAdmin: boolean
-      token: string
-    }
-    next()
+      _id: string;
+      name: string;
+      email: string;
+      isAdmin: boolean;
+      token: string;
+    };
+    next();
   } else {
-    res.status(401).json({ message: 'No Token' })
+    res.status(401).json({ message: "No Token" });
   }
-}
+};
